0af930e8fc40ff657b6c3566671ca9b53d031a50
Ajouad <96442128+Ajouad@users.noreply.github.com>
2023-10-23 15:44:50 +0200

    Fixing files

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -33,8 +33,7 @@ def main(trainset_path: Path) -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Train a model using the data at the given path.")
+    parser = argparse.ArgumentParser(description="Train a model using the data at the given path.")
     parser.add_argument(
         "trainset_path",
         type=Path,
