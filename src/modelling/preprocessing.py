306fed97d0f53a80732f324be183d384821ddc9d
Samuel Berrebi <samuel.berrebi@polytechnique.edu>
2023-10-23 14:59:16 +0200

    Fixing merge issues - main and preprocessing

---
+++ src/modelling/preprocessing.py
@@ -0,0 +1,26 @@
+from typing import List, Tuple
+
+import numpy as np
+import pandas as pd
+import scipy.sparse
+#from config import CATEGORICAL_COLS
+from loguru import logger
+from prefect import flow, task
+from sklearn.feature_extraction import DictVectorizer
+
+# @task
+# ef compute_target
+
+
+# @task
+# def filter_outliers
+
+
+# @task
+# def encode_categorical_cols
+
+# @task
+# def extract_x_y
+
+# @flow(name="Preprocess data")
+# def process_data#
